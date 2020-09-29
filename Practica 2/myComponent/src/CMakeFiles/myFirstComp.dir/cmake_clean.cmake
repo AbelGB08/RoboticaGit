90355428d70d1c80ad8be584e8d53ff0beb77444
julian1895 <jredondoy@alumnos.unex.es>
2020-09-29 16:56:44 +0200

    Practica 2

---
+++ Practica 2/myComponent/src/CMakeFiles/myFirstComp.dir/cmake_clean.cmake
@@ -0,0 +1,34 @@
+file(REMOVE_RECURSE
+  "../bin/myFirstComp"
+  "../bin/myFirstComp.pdb"
+  "CMakeFiles/myFirstComp_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/myFirstComp_autogen.dir/ParseCache.txt"
+  "myFirstComp_autogen"
+  "CMakeFiles/myFirstComp.dir/CommonBehavior.cpp.o"
+  "CMakeFiles/myFirstComp.dir/DifferentialRobot.cpp.o"
+  "CMakeFiles/myFirstComp.dir/GenericBase.cpp.o"
+  "CMakeFiles/myFirstComp.dir/Laser.cpp.o"
+  "CMakeFiles/myFirstComp.dir/commonbehaviorI.cpp.o"
+  "CMakeFiles/myFirstComp.dir/genericmonitor.cpp.o"
+  "CMakeFiles/myFirstComp.dir/genericworker.cpp.o"
+  "CMakeFiles/myFirstComp.dir/main.cpp.o"
+  "CMakeFiles/myFirstComp.dir/myFirstComp_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/myFirstComp.dir/opt/robocomp/classes/qlog/qlog.cpp.o"
+  "CMakeFiles/myFirstComp.dir/opt/robocomp/classes/rapplication/rapplication.cpp.o"
+  "CMakeFiles/myFirstComp.dir/opt/robocomp/classes/sigwatch/sigwatch.cpp.o"
+  "CMakeFiles/myFirstComp.dir/specificmonitor.cpp.o"
+  "CMakeFiles/myFirstComp.dir/specificworker.cpp.o"
+  "CommonBehavior.cpp"
+  "CommonBehavior.h"
+  "DifferentialRobot.cpp"
+  "DifferentialRobot.h"
+  "GenericBase.cpp"
+  "GenericBase.h"
+  "Laser.cpp"
+  "Laser.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myFirstComp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
