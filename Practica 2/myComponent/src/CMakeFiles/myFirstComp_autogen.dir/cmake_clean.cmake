a05c081373e6212bdda22733569a4b39e9f7649b
julian1895 <jredondoy@alumnos.unex.es>
2020-10-20 17:07:00 +0200

    asdf

--- Practica 2/myComponent/src/CMakeFiles/myFirstComp_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/myFirstComp_autogen"
-  "myFirstComp_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/myFirstComp_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
