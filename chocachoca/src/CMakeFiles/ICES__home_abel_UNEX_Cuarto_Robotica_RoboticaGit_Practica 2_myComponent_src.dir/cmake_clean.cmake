de4b6b3274977a4e3fa5adb9a77d13f95515ab1e
julian1895 <jredondoy@alumnos.unex.es>
2020-10-20 17:26:29 +0200

    asd

--- chocachoca/src/CMakeFiles/ICES__home_abel_UNEX_Cuarto_Robotica_RoboticaGit_Practica 2_myComponent_src.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ICES__home_abel_UNEX_Cuarto_Robotica_RoboticaGit_Practica 2_myComponent_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
